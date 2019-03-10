c0491a0092f0a64669c3d954db711b47ae0c80d2
mlnchk <danil.mlnchk@gmail.com>
2019-03-10 20:36:07 +0300

    Configuration files

---
+++ .vimrc
@@ -0,0 +1,11 @@
+syntax enable
+
+set tabstop=2
+set softtabstop=2
+set expandtab
+
+set number
+set showcmd
+set cursorline
+
+set showmatch
--- init.sh
+++ init.sh
@@ -1,5 +1,11 @@
-mkdir -p "$(HOME)/code" && cd "$(HOME)/code";
+# mkdir -p "$(HOME)/code" && cd "$(HOME)/code";
 git clone https://github.com/mlnchk/dotfiles.git && cd dotfiles;
 # sudo pip install ansible && ansible-playbook playbook.yml;
 
 brew bundle;
+# ln -s "${HOME}/dotfiles/.bash_profile" "${HOME}/.bash_profile"
+# ln -s "${HOME}/dotfiles/.gitignore_global" "${HOME}/.gitignore_global"
+ln -s "${HOME}/dotfiles/.zshrc" "${HOME}/.zshrc"
+ln -s "${HOME}/dotfiles/.vimrc" "${HOME}/.vimrc"
+ln -s "${HOME}/dotfiles/.npmrc" "${HOME}/.npmrc"
+ln -s "${HOME}/dotfiles/.gitconfig" "${HOME}/.gitconfig"
