cf7562af618e6dc95922d5473104a462e900674e
Danil <danil.mlnchk@gmail.com>
2019-04-26 14:41:56 +0300

    Update init.sh

--- init.sh
+++ init.sh
@@ -3,9 +3,9 @@ git clone https://github.com/mlnchk/dotfiles.git && cd dotfiles;
 # sudo pip install ansible && ansible-playbook playbook.yml;
 
 brew bundle;
-# ln -s "${HOME}/dotfiles/.bash_profile" "${HOME}/.bash_profile"
-# ln -s "${HOME}/dotfiles/.gitignore_global" "${HOME}/.gitignore_global"
-ln -s "${HOME}/dotfiles/.zshrc" "${HOME}/.zshrc"
-ln -s "${HOME}/dotfiles/.vimrc" "${HOME}/.vimrc"
-ln -s "${HOME}/dotfiles/.npmrc" "${HOME}/.npmrc"
-ln -s "${HOME}/dotfiles/.gitconfig" "${HOME}/.gitconfig"
+# ln -s "${HOME}/code/dotfiles/.bash_profile" "${HOME}/.bash_profile"
+# ln -s "${HOME}/code/dotfiles/.gitignore_global" "${HOME}/.gitignore_global"
+ln -s "${HOME}/code/dotfiles/.zshrc" "${HOME}/.zshrc"
+ln -s "${HOME}/code/dotfiles/.vimrc" "${HOME}/.vimrc"
+ln -s "${HOME}/code/dotfiles/.npmrc" "${HOME}/.npmrc"
+ln -s "${HOME}/code/dotfiles/.gitconfig" "${HOME}/.gitconfig"
